2c37fb49672708a1569edb2f99b70ef3b022b05b
T1505SR <samuel.rocha1@external.stellantis.com>
2025-03-21 08:57:13 -0300

    no message

--- MoveFile.py
+++ MoveFile.py
@@ -1,9 +1,12 @@
 import shutil
 import os
 
-# Caminhos das pastas e do arquivo
-arquiv_orig = 'Architecture/file.txt'
-arquiv_dest = 'Code/file.txt'
+# Diretório base onde o script está rodando
+BASE_DIR = os.getenv("TEAMCITY_BUILD_CHECKOUTDIR", "Z:/buildAgent/work/5481815137319f3c")
+
+# Caminhos completos
+arquiv_orig = os.path.join(BASE_DIR, "Architecture", "file.txt")
+arquiv_dest = os.path.join(BASE_DIR, "Code", "file.txt")
 
 # Verifica se o arquivo existe na pasta de origem
 if os.path.exists(arquiv_orig):
